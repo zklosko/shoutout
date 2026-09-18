d3b898df67817ae074b58918ef408508b812ea19
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-18 10:59:09 -0400

    adding FormSelect component

---
+++ src/lib/components/FormSelect.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let {
+		label,
+		bindValue = $bindable(''),
+		onChange,
+		noSelectionText,
+		options
+	}: {
+		label: string;
+		bindValue: string;
+		onChange?: () => void;
+		noSelectionText: string;
+		options: { uuid: string; name: string }[];
+	} = $props();
+</script>
+
+<div class="field">
+	<label class="label">{label}</label>
+	<div class="control">
+		<div class="select is-fullwidth">
+			<select bind:value={bindValue} onchange={onChange}>
+				<option value="" disabled>{noSelectionText}</option>
+				{#each options as o}
+					<option value={o.uuid}>{o.name}</option>
+				{/each}
+			</select>
+		</div>
+	</div>
+</div>
