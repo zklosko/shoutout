41215967a30c309e826a90af5f3a22775c57e0f4
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-17 21:39:12 -0400

    ran prettier

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,19 +10,16 @@
 					<h1 class="title">{page.status}</h1>
 
 					<p class="is-text-7 has-color-grey">
-					{#if page.status === 404}
-						That page doesn't exist.
-					{:else}
-						{page.error?.message ?? 'Something went wrong.'}
-					{/if}
+						{#if page.status === 404}
+							That page doesn't exist.
+						{:else}
+							{page.error?.message ?? 'Something went wrong.'}
+						{/if}
 					</p>
 
-					<a href="/" class="button mt-4">
-						Back home
-					</a>
+					<a href="/" class="button mt-4"> Back home </a>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-
