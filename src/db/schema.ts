8caab0eb01629614d534bc29d0de1a8b82a9fbc7
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-23 21:15:11 -0400

    adding connections and companion settings tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,6 +9,21 @@ export const childCodesTable = sqliteTable("child_codes", {
 
 export const settingsTable = sqliteTable("settings", {
     id: text().notNull().$default(() => "settings"),
-    infoText: text(),
+    infoText: text().notNull().$default(() => ""),
     approverPassword: text().notNull(),
+    port: int().notNull().$default(() => 8080)
+})
+
+export const connectionsTable = sqliteTable("connections", {
+    type: text().notNull().$default(() => "companion"),
+    host: text().notNull(),
+    port: int().notNull().$default(() => 8000)
+})
+
+export const companionButtonsTable = sqliteTable("companion_buttons", {
+    id: int().primaryKey({autoIncrement: true}),
+    page: int().notNull().$default(() => 1),
+    row: int().notNull().$default(() => 0),
+    col: int().notNull().$default(() => 0),
+    variableName: text().notNull(),
 })
