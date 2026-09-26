571c00f9a2d8325d3d2e19d6bf1a54707dd44c24
zklosko <31484611+zklosko@users.noreply.github.com>
2026-09-26 09:52:05 -0400

    adding infoText message to dashboard, cleaning up styles

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 
-// You can specify any property from the libsql connection options
 export const db = drizzle({ connection: { url: process.env.DB_FILE_NAME! } });
--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -1,12 +1,14 @@
 import type { FastifyInstance } from "fastify";
 import { db } from "../db/index.js";
-import { childCodesTable } from "../db/schema.js";
+import { childCodesTable, settingsTable } from "../db/schema.js";
 
 export async function dashboardRoutes(fastify: FastifyInstance, options: {}) {
   fastify.get("/", async (request, response) => {
     const childCodes = await db.select().from(childCodesTable).all();
+    const infoText = await db.select({ text: settingsTable.infoText }).from(settingsTable).get()
     return response.viewAsync("dashboard.hbs", {
       childCodes,
+      infoText: infoText?.text,
       isAuthenticated: request.isAuthenticated(),
     });
   });
